2a90a08e4c5d879ba96b5e12a78af3c0729e9bbf
Moldovan Andrei <moldovanandrei8399@gmail.com>
2021-02-24 15:01:56 +0200

    Improved responsive navbar

--- components/Header.js
+++ components/Header.js
@@ -1,194 +1,239 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 import _ from 'lodash'
-import React from 'react'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import { Menu as MenuIcon } from '@material-ui/icons'
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import HomeIcon from '@material-ui/icons/Home'
+import InfoIcon from '@material-ui/icons/Info'
+import BrushIcon from '@material-ui/icons/Brush'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import PhoneIcon from '@material-ui/icons/Phone';
+import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
-const navLinks = [
-    { label: 'Home', url: '/', isSecured: false },
-    { label: 'About', url: '/about', isSecured: false },
-    { label: 'Handmade', url: '/handmade', isSecured: false },
-    { label: 'Manufactured', url: '/manufactured', isSecured: false },
-    { label: 'Contact', url: '/contact', isSecured: false },
-    { label: 'Admin', url: '/admin', isSecured: true, requiresAdmin: true },
-    { label: 'Account', url: '/account', isSecured: true },
-]
-
-
-const NavLinks = ({ navLinks }) => {
-    return _.map(navLinks, ({ label, url, isSecured, requiresAdmin }) => {
-        return (
-            <li>{label}</li>
-        )
-    })
-}
-
-
-
-
-const drawerWidth = 240;
+const drawerWidth = 200;
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: 'flex',
-    },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
+        display: "flex"
     },
     appBar: {
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
-        },
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
     },
     menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
+        marginRight: 36
     },
-    // necessary for content to be below app bar
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
+    hide: {
+        display: "none"
+    },
+    drawer: {
         width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: "nowrap"
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    drawerClose: {
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        overflowX: "hidden",
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up("sm")]: {
+            width: theme.spacing(9) + 1
+        }
+    },
+    toolbar: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar
     },
     content: {
         flexGrow: 1,
-        padding: theme.spacing(3),
-    },
+        padding: theme.spacing(3)
+    }
 }));
 
-function Header(props) {
-    const { window } = props;
+const navLinks = [
+    { label: 'Home', url: '/', icon: <HomeIcon />, isSecured: false },
+    { label: 'About', url: '/about', icon: <InfoIcon />, isSecured: false },
+    { label: 'Handmade', url: '/handmade', icon: <BrushIcon />, isSecured: false },
+    { label: 'Manufactured', url: '/manufactured', icon: <BusinessCenterIcon />, isSecured: false },
+    { label: 'Contact', url: '/contact', icon: <PhoneIcon />, isSecured: false },
+    { label: 'Admin', url: '/admin', icon: <SettingsApplicationsIcon />, isSecured: true, requiresAdmin: true },
+    { label: 'Account', url: '/account', icon: <AccountCircleIcon />, isSecured: true },
+]
+
+const NavLinks = ({ navLinks }) => {
+    return _.map(navLinks, ({ label, url, icon, isSecured, requiresAdmin }) => {
+        if (!isSecured) {
+            return (
+                <ListItem button key={label}>
+                    <ListItemIcon>
+                        {icon}
+                    </ListItemIcon>
+                    <ListItemText primary={label} />
+                </ListItem>
+            )
+        }
+    })
+}
+
+const RestrictedLinks = ({ navLinks }) => {
+    return _.map(navLinks, ({ label, url, icon, isSecured, requiresAdmin }) => {
+        if (isSecured) {
+            return (
+                <ListItem button key={label}>
+                    <ListItemIcon>
+                        {icon}
+                    </ListItemIcon>
+                    <ListItemText primary={label} />
+                </ListItem>
+            )
+        }
+    })
+}
+
+const Header = () => {
     const classes = useStyles();
     const theme = useTheme();
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
+    const handleDrawerOpen = () => {
+        setOpen(true);
     };
 
-    const drawer = (
-        <div>
-            <div className={classes.toolbar} />
-            <Divider />
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </div>
-    );
-
-    const container = window !== undefined ? () => window().document.body : undefined;
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     return (
         <div className={classes.root}>
             <CssBaseline />
-            <AppBar position="fixed" className={classes.appBar}>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open
+                })}
+            >
                 <Toolbar>
                     <IconButton
                         color="inherit"
                         aria-label="open drawer"
+                        onClick={handleDrawerOpen}
                         edge="start"
-                        onClick={handleDrawerToggle}
-                        className={classes.menuButton}
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: open
+                        })}
                     >
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" noWrap>
-                        Responsive drawer
+                        Mini variant drawer
           </Typography>
                 </Toolbar>
             </AppBar>
-            <nav className={classes.drawer} aria-label="mailbox folders">
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Hidden smUp implementation="css">
-                    <Drawer
-                        container={container}
-                        variant="temporary"
-                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        variant="permanent"
-                        open
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-            </nav>
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open
+                    })
+                }}
+            >
+                <div className={classes.toolbar}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === "rtl" ? (
+                            <ChevronRightIcon />
+                        ) : (
+                            <ChevronLeftIcon />
+                        )}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <NavLinks navLinks={navLinks} />
+                </List>
+                <Divider />
+                <List>
+                    <RestrictedLinks navLinks={navLinks} />
+                </List>
+            </Drawer>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
                 <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-        </Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+                    dolor purus non enim praesent elementum facilisis leo vel. Risus at
+                    ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+                    quisque non tellus. Convallis convallis tellus id interdum velit
+                    laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+                    adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+                    integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+                    eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+                    quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+                    vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+                    lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+                    faucibus et molestie ac.
+                </Typography>
                 <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+                    ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+                    elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+                    sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+                    mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+                    risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+                    purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+                    tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+                    morbi tristique senectus et. Adipiscing elit duis tristique
+                    sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                    posuere sollicitudin aliquam ultrices sagittis orci a.
+                </Typography>
             </main>
         </div>
     );
 }
-
 
 export default Header

58d2708aca56de47818eb92cb0ab3ab9ce73c41c
vladcocis <vladcocis@gmail.com>
2021-02-24 12:33:01 +0200

    Demo files

---
+++ pages/demo/[derp]/[muie].js
@@ -0,0 +1,5 @@
+const Muie = () => {
+    return <h1>Muie</h1>
+}
+
+export default Muie
---
+++ pages/demo/index.js
@@ -0,0 +1,7 @@
+const DemoIndex = () => {
+    return (
+        <h1>Demo Index</h1>
+    )
+}
+
+export default DemoIndex

e8c3f71db1089f8946f3cf9de0f5c7369e64c7d6
Roxana Pop <61201993+roxanapopelena@users.noreply.github.com>
2021-03-07 16:29:04 +0000

    Front-end implementation of the account section of the system
    Implemented the sign-in and sign-up forms:
    -created the different components
    -created 2 pages in the pages folder
    -linked the sign-in form to the navbar
    -linked the sign-up form to the sign-in component (users can click on a link if they do not have and account; the link will redirect them to the sign-up page)

---
+++ pages/account.js
@@ -0,0 +1,11 @@
+import SignInSide from "../components/SignInSide";
+
+const account =() =>{
+    return(
+        <div>
+            <SignInSide />
+        </div>
+    )
+}
+
+export default account;
---
+++ pages/signup.js
@@ -0,0 +1,12 @@
+import SignInSide from "../components/SignInSide";
+import SignUp from "../components/SignUp";
+
+const account =() =>{
+    return(
+        <div>
+            <SignUp />
+        </div>
+    )
+}
+
+export default account;

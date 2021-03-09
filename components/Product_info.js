e77dbdb7756a9a4813d3f33b5b27db824f81bd7d
sukrudurak <61364096+sukrudurak@users.noreply.github.com>
2021-03-09 09:52:50 +0000

    product info page sorted and  commentsjs added

--- components/Product_info.js
+++ components/Product_info.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import {useEffect, useState} from 'react';
-
+import { useEffect, useState} from 'react';
 
 import {
-    Card,
-    CardContent,
-    CardMedia,
-    CardHeader,
-    CardActions,
-    Grid,
-    Button,
-    Typography,
+  Card,
+	CardContent,
+  CardHeader,
+  CardActions,
+	Grid,
+	Button,
+	Typography,
+  Container,
+  Paper,
 } from '@material-ui/core'
 
-import {makeStyles} from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import {red} from '@material-ui/core/colors';
+import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -29,99 +29,210 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: 345,
-        marginTop: '13%',
-        marginLeft: '20%',
-    },
-    media: {
-        height: 350,
-        paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    avatar: {
-        backgroundColor: red[500],
-    },
-    header: {
-        flexDirection: 'column',
-        alignItems: 'center',
+import {
+  CardMedia,
+} from "@material-ui/core";
 
-    },
-    sub: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        flex: 1,
-        color: 'black'
+const productStyle = makeStyles({
+ root: {
+   marginTop: "7rem",
+   marginBottom: "4rem",
 
-    },
+ },
+ inStock: {
+   marginBottom: "4rem",
+   color:"blue",
+ },
+ productDescription: {
+   marginBottom: "2rem",
+ },
+ productImage: {
+   width: "100%",
+   height: "100%",
+   backgroundSize: "100% 200",
+   minHeight: "30vh",
+ },
+ price: {
+   marginBottom: "1rem",
+    fontSize:'20px',
 
-    leftdiv: {
+ },
+ arrowBtn: {
+   maxWidth: "fit-content",
+   marginTop: "0.3rem",
+ },
 
 
-        maxWidth: "50%",
-        marginTop: '1%',
-        marginLeft: '1%',
-        flexDirection: 'column',
-        display: "flex",
 
-    },
+imgcontainer:{
+  maxWidth:'50%',
 
-    img: {
+},
 
-        maxWidth: "100%",
+productHeader:{
 
+ fontSize:'40px',
 
-    },
+},
 
-    img: {
+strepper:{
+ marginTop: "2rem",
 
-        maxWidth: '50%',
-        marginLeft: '20%',
-        marginRight: '20%',
-        flexDirection: 'column',
-        alignItems: 'center',
+},
 
 
-    },
+
+});
 
 
-}));
+const tutorialSteps = [
+  {
+
+    imgPath:
+      'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a46742ce-8dcf-4540-ba45-7ed7b35a9a99/air-zoom-pegasus-37-premium-koşu-ayakkabısı-lRcGVd.jpg',
+  },
+  {
+
+    imgPath:
+      'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/fe89bbe0-c5d9-4de7-99a4-fd3845693673/air-zoom-pegasus-37-premium-koşu-ayakkabısı-lRcGVd.jpg',
+  },
+  {
+
+    imgPath:
+      'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/9ef5bc51-9e9f-40a7-86a4-13d746b8a11b/air-force-1-react-ayakkabısı-VF4bwV.jpg',
+  },
+  {
+
+    imgPath:
+      'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/w_432,c_limit/ce13ce3b-704c-4bc7-bad2-9fc214a8079a/air-max-plus-3-ayakkabısı-br9zBx.jpg',
+  },
+  {
+
+    imgPath:
+      'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d02b66b7-ee6a-429a-a0e0-63c7da600c49/air-jordan-35-basketbol-ayakkabısı-bGv0b6.jpg',
+  },
+];
 
 
-const Product_info = ({product, product_images, product_eviews}) => {
-
-    const classes = useStyles();
 
 
-    return (
-        <div>
+const Product_info = ({ data_product, product_images}) => {
+
+const pclasses = productStyle();
 
 
-            <h1>s</h1>
+const [activeStep, setActiveStep] = React.useState(0);
+const maxSteps = tutorialSteps.length;
 
-            <div>
+const handleNext = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep + 1);
+};
 
-
-                <img className={classes.img,classes.img}
-                     src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/72a58094-d560-41f7-80a3-7f16385325d0/air-vapormax-2020-fk-ayakkabısı-BhRhR9.jpg"/>
-
-            </div>
-
-
-        </div>
+const handleBack = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep - 1);
+};
 
 
-    )
+     return(
+
+
+       <Container >
+
+         <Grid container spacing={4}>
+           <Grid item xs={12} md={6}>
+
+
+
+           <div>
+             <Paper square elevation={0} >
+               <Typography></Typography>
+             </Paper>
+             <center>
+             <img
+             className={pclasses.imgcontainer}
+
+               src={tutorialSteps[activeStep].imgPath}
+               alt={tutorialSteps[activeStep].label}
+             />
+             </center>
+             <MobileStepper
+             className={pclasses.strepper}
+               steps={maxSteps}
+               position="static"
+               variant="text"
+               activeStep={activeStep}
+               nextButton={
+                 <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                   Next
+
+                 </Button>
+               }
+               backButton={
+                 <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+
+                   Back
+                 </Button>
+               }
+             />
+           </div>
+
+
+
+           </Grid>
+           <Grid container item xs={12} md={6} className={pclasses.productHeader} direction="column">
+             <Grid item>
+               <Typography variant="h4"> </Typography>
+                NIKE
+               <Typography
+                 variant="caption"
+                 display="block"
+                 color="secondary"
+                 className={pclasses.inStock}
+               >
+                 In Stock
+               </Typography>
+             </Grid>
+             <Typography variant="body2" className={pclasses.productDescription}>
+               In the ‘70s, Nike was the new shoe on the block. So new in fact, we were still breaking into the basketball scene and testing prototypes on the feet of our local team. Of course, the design improved over the years, but the name stuck. The Nike Blazer Mid ’77 Vintage—classic since the beginning.
+             </Typography>
+
+             <Typography variant="subtitle2" className={pclasses.price}>
+               1300 $
+             </Typography>
+
+             <Button
+               variant="contained"
+               color="primary"
+
+             >
+              Buy
+             </Button>
+
+             <Grid
+               item
+               container
+               justify="space-between"
+               alignItems="center"
+
+             >
+               <IconButton
+                 className={pclasses.arrowBtn}
+                 color="primary"
+                 onClick={() => history.goBack()}
+               >
+
+               </IconButton>
+               <IconButton onClick={() => addToFavs(product)}>
+
+               </IconButton>
+             </Grid>
+           </Grid>
+         </Grid>
+       </Container>
+
+
+
+     )
 
 
 }
--- components/Products_list_card.js
+++ components/Products_list_card.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import {useEffect, useState} from 'react';
+import { useEffect, useState} from 'react';
 
 
 import {
-    Card,
-    CardContent,
-    CardMedia,
-    CardHeader,
-    CardActions,
-    Grid,
-    Button,
-    Typography,
+  Card,
+	CardContent,
+	CardMedia,
+  CardHeader,
+  CardActions,
+	Grid,
+	Button,
+  Container,
+	Typography,
 } from '@material-ui/core'
 
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import {red} from '@material-ui/core/colors';
+import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -30,107 +31,116 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: '35%',
-
-        marginTop: '13%',
-        marginLeft: '5%',
-        marginRight: '5%',
-    },
-    media: {
-        height: 350,
-        paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    avatar: {
-        backgroundColor: red[500],
-    },
-    header: {
-        flexDirection: 'column',
-        alignItems: 'center',
-
-    },
-    sub: {
-        flexDirection: 'column',
-        alignItems: 'center',
-
-        color: 'black',
+  root: {
 
 
-    },
+  },
+  media: {
+    height: 350,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  header:{
+    flexDirection: 'column',
+   alignItems: 'center',
 
-    shop: {
-        align: 'right',
-        textAlign: 'right',
-    },
+  },
+  sub:{
+    flexDirection: 'column',
+   alignItems: 'center',
+
+   color:'black',
+
+
+
+  },
+
+  shop:{
+  align: 'right',
+   textAlign:'right',
+  },
+
 
 
 }));
 
 
-const Products_list_card = ({product}) => {
+
+const Products_list_card = ({ product }) => {
 
 
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
+     const classes = useStyles();
+     const [expanded, setExpanded] = React.useState(false);
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+     const handleExpandClick = () => {
+       setExpanded(!expanded);
+     };
 
-    const handleClick = (event) => {
+     const handleClick = (event) =>  {
         event.preventDefault();
-        console.log('Free pizza!');
-        console.log(event);
-    }
+         console.log('Free pizza!');
+         console.log(event);
+     }
 
 
-    return (
+     return(
+
+       <Container >
 
 
-        <Link href={'/product/' + product.id}>
-            <Card className={classes.root}>
-                <CardHeader className={classes.header}
+           <Grid item xs={12} md={4}>
+
+   <Link  href={'/product/'+product.id } >
+     <Card className={classes.root}>
+         <CardHeader className={classes.header}
 
 
-                            title={product.product_name}
+           title={product.product_name}
 
-                />
-                <CardMedia
-                    className={classes.media}
-                    image={product.showroomImg}
-                    title="Paella dish"
-                />
-                <CardContent className={classes.sub}>
-                    <Typography className={classes.sub} variant="body2" color="textSecondary" component="p">
-                        {product.category_id}
-                    </Typography>
-                    {product.product_desc}
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <FavoriteIcon/>
-                    </IconButton>
-                    <IconButton className={classes.shop} onClick={handleClick} aria-label="add to shopping cart">
-                        <AddShoppingCartIcon onClick={handleClick}/>
-                    </IconButton>
+         />
+         <CardMedia
+           className={classes.media}
+           image={product.showroomImg}
+           title="Paella dish"
+         />
+         <CardContent className={classes.sub}>
+           <Typography  className={classes.sub} variant="body2" color="textSecondary" component="p">
+             {product.category_id}
+           </Typography>
+          {product.product_desc}
+         </CardContent>
+         <CardActions disableSpacing>
+           <IconButton aria-label="add to favorites">
+             <FavoriteIcon />
+           </IconButton>
+           <IconButton className={classes.shop} onClick={handleClick} aria-label="add to shopping cart">
+       <AddShoppingCartIcon onClick={handleClick} />
+       </IconButton>
 
 
-                </CardActions>
+         </CardActions>
 
-            </Card>
-        </Link>
+       </Card>
+</Link>
 
-    )
+
+</Grid>
+</Container>
+     )
+
+
 
 
 }

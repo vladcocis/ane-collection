4ffc56972f62acf39e876a284ccd7cc82f8297bd
sukrudurak <61364096+sukrudurak@users.noreply.github.com>
2021-03-09 10:07:02 +0000

    product commet add

---
+++ components/Product_comment.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    marginLeft:'1%',
+    marginRight:'1%',
+    marginTop:'5%',
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+
+  header:{
+    marginLeft:'2rem',
+
+  },
+
+}));
+
+const Product_comment = ({ comments }) => {
+
+
+  const classes = useStyles();
+
+
+
+  return (
+<div>
+
+
+    <List className={classes.root}>
+    <h1 className={classes.header} >Comments</h1>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://isbh.tmgrup.com.tr/sbh/2014/11/17/1416219020478.png" />
+        </ListItemAvatar>
+        <ListItemText
+          primary={comments.name}
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                {comments.comment}
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+
+
+        />
+
+
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="https://upload.wikimedia.org/wikipedia/commons/1/17/Avril_Lavigne%2C_Wango_Tango_2013.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="https://images.bursadabugun.com/haber/2018/11/02/1030630-avril-lavigne-olmedim-yasiyorum-5bdc63904aa93.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Oui Oui"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Sandra Adams
+              </Typography>
+              {' — Do you have Paris recommendations? Have you ever…'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+
+
+
+
+</div>
+
+  );
+}
+
+export default Product_comment;
